13e4675d8fd7c967460a6ff54a9afb2166e02e16
sadtimoha377 <makartim112@gmail.com>
2026-02-09 23:41:47 +0200

    File.txt

---
+++ Patterns/TaskNumOne.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+/* public class AppConfig
+{
+   private static AppConfig _instance;
+   private static readonly object _lock = new object();
+   private Dictionary<string, string> _settings;
+
+   private AppConfig()
+   {
+       _settings = new Dictionary<string, string>();
+   }
+
+   public static AppConfig Instance
+   {
+       get
+       {
+           if (_instance == null)
+           {
+               lock (_lock)
+               {
+                   if (_instance == null)
+                       _instance = new AppConfig();
+               }
+           }
+           return _instance;
+       }
+   }
+
+   public void SetSetting(string key, string value)
+   {
+       _settings[key] = value;
+   }
+
+   public string GetSetting(string key)
+   {
+       if (_settings.ContainsKey(key))
+           return _settings[key];
+
+       return null;
+   }
+}
+class TaskNumOne
+{
+   static void Main()
+   {
+       AppConfig config = AppConfig.Instance;
+
+       config.SetSetting("Theme", "Dark");
+       config.SetSetting("Language", "English");
+
+       Console.WriteLine(config.GetSetting("Theme"));
+       Console.WriteLine(config.GetSetting("Language"));
+   }
+}  */
---
+++ Patterns/TaskNumTwo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public interface IShape
+{
+    void Draw();
+}
+
+public class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a circle");
+    }
+}
+
+public class Rectangle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle");
+    }
+}
+
+public abstract class ShapeFactory
+{
+    public abstract IShape CreateShape();
+}
+
+public class CircleFactory : ShapeFactory
+{
+    public override IShape CreateShape()
+    {
+        return new Circle();
+    }
+}
+
+public class RectangleFactory : ShapeFactory
+{
+    public override IShape CreateShape()
+    {
+        return new Rectangle();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ShapeFactory circleFactory = new CircleFactory();
+        IShape circle = circleFactory.CreateShape();
+        circle.Draw();
+
+        ShapeFactory rectangleFactory = new RectangleFactory();
+        IShape rectangle = rectangleFactory.CreateShape();
+        rectangle.Draw();
+    }
+}
